7abf8aa6e3893f411f249c3542bd2b82d0a16ad5
Adrian <adry.dabp@gmail.com>
2018-11-18 18:06:01 -0400

    primeros cambios

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="menu">
+    HOlaaaaaa
+  </section>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+</style>
